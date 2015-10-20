d0e467a08b3088e0d9ddde1c51a0681eeabf8e90
Guillaume Perrin <gperrin01@gmail.com>
2015-10-20 13:26:57 +0100

    adding buttons so that students cannot book more than one instructor & can cancel their booking - need to change state on addBooking so that student can add topic

--- www/js/controllers/HomeCtrl.js
+++ www/js/controllers/HomeCtrl.js
@@ -2,13 +2,52 @@ angular.module('starter.controllers')
 
 .controller('HomeCtrl', function($scope) {
 
-  $scope.instructors = [ 
-    { name: 'Tom', status:'Available', queue: 0 },
-    { name: 'Gui', status:'Available',queue: 0 },
-    { name: 'Jen', status:'Busy', queue: 3 },
-    { name: 'Jeremy', status:'Busy', queue: 4 }
-  ];
+  // make an Instructor Resource
 
-  
+  $scope.instructors = [ 
+    { name: 'Tom', status:'Available', bookings: [] },
+    { name: 'Gui', status:'Available', bookings: [] },
+    { name: 'Jen', status:'Busy', bookings: [{name: 'jack', booked: ""}, {name: 'rob', booked: ""}] },
+    { name: 'Jeremy', status:'Busy', bookings: [{name: 'chris', booked: ""}, {name: 'mark', booked: ""}, {name: 'paco', booked: ""}] }
+  ];
+  // $scope.instructors = Instructor.query();
+
+  // ensure access to current user
+  // Parse.currentUser();
+  // fake current user for now
+  $scope.currUser = {name: 'Test Donkey', booked: ""};
+
+  $scope.toggleQueue = function (instructor) {
+    // ensure the divs know when to show and hide
+    // first hide them all, then show the one we want
+    $scope.instructors.forEach(function(instructor){
+      instructor.showMe = false;
+    })
+    instructor.showMe === true ? instructor.showMe = false : instructor.showMe = true;
+  }
+
+  $scope.addBooking = function (instructor) {
+    // SEND TO SERVER
+    // PUT on instructor, create a booking and add it to instructor.bookings
+    // assuming succesful response
+
+    // MUST TAKE YU TO A NEW PAGE
+    
+    instructor.bookings.push($scope.currUser);
+    instructor.status = 'busy';
+    $scope.currUser.booked = instructor.name;
+  }
+
+  $scope.cancelBooking = function (instructor) {
+    // SEND TO SERVER
+    // PUT on instructor, delete a booking and remove it from instructor.bookings
+    // assuming succesful response
+    $scope.currUser.booked = "";
+    var indexArray = instructor.bookings.indexOf($scope.currUser);
+    instructor.bookings.splice(indexArray, 1);
+  }
 
 })
+
+
+
